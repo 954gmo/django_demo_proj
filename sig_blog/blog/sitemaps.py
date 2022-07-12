97e0c7a55c8a15aaa8aa8e94709ef43c7b8d3524
sixdigit <mog@sixdigit.net>
2022-07-12 10:27:58 -0400

    stiempas

---
+++ sig_blog/blog/sitemaps.py
@@ -0,0 +1,21 @@
+# -*- encoding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__ENTITY_author__ = "SIX DIGIT INVESTMENT GROUP"
+__author__ = "GWONGZAN"
+
+from django.contrib.sitemaps import Sitemap
+from .models import Post
+
+
+class PostSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
+        return Post.published.all()
+
+    def lastmod(self, obj):
+        return obj.updated
--- sig_blog/sig_blog/settings.py
+++ sig_blog/sig_blog/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
 
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
